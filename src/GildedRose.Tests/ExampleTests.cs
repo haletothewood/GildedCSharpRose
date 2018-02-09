dcb7246edf965b5861e19cd1cce794e3e08b4e3d
David Halewood <davidnhalewood@hotmail.com>
2018-02-09 17:13:08 +0000

    Add test for Normal Item

--- src/GildedRose.Tests/ExampleTests.cs
+++ src/GildedRose.Tests/ExampleTests.cs
@@ -11,17 +11,41 @@ namespace GildedRose.Tests
     public class UpdateItemsTests
     {
         [Test]
-        public void UpdatedQuality_QualityIsReducedBy1()
+        public void NormalItem_UpdatedQuality()
         {
             var items = new List<Item>
             {
-                new Item {Name = "Normal", SellIn = 10, Quality = 10},
+                new Item {Name = "Normal", SellIn = 10, Quality = 20},
             };
 
             var app = new Program(items);
 
             app.UpdateQuality();
-            Assert.That(app.Items[0].Quality, Is.EqualTo(9));
+
+            /*Quality reduces by 1*/
+            Assert.That(app.Items[0].Quality, Is.EqualTo(19));
+
+            /*SellIn reduces by 1*/
+            Assert.That(app.Items[0].SellIn, Is.EqualTo(9));
+
+            for (var i = 0; i < 10; i++)
+            {
+                app.UpdateQuality();
+            }
+
+            /*Quality reduces at twice the rate if SellIn is less than zero*/
+            Assert.That(app.Items[0].Quality, Is.EqualTo(8));
+
+            /*SellIn by 1 each day*/
+            Assert.That(app.Items[0].SellIn, Is.EqualTo(-1));
+
+            for (int i = 0; i < 100; i++)
+            {
+                app.UpdateQuality();
+            }
+
+            /*Quality cannot be less than zero*/
+            Assert.That(app.Items[0].Quality, Is.EqualTo(0));
         }
     }
 }

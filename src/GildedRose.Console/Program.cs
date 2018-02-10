b5990a356c834e8fa14ac7bef7a04dcc7f36a27b
David Halewood <davidnhalewood@hotmail.com>
2018-02-10 13:47:58 +0000

    Extract Aged Brie update into private function

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -39,55 +39,15 @@ namespace GildedRose.Console
 
         public void UpdateQuality()
         {
+
             for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                switch (Items[i].Name)
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    case "Aged Brie":
+                        UpdateBrieItem(Items[i]);
+                        break;
+                    default:
+                        if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
                             if (Items[i].Quality > 0)
                             {
@@ -99,18 +59,76 @@ namespace GildedRose.Console
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+
+                                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                                {
+                                    if (Items[i].SellIn < 11)
+                                    {
+                                        if (Items[i].Quality < 50)
+                                        {
+                                            Items[i].Quality = Items[i].Quality + 1;
+                                        }
+                                    }
+
+                                    if (Items[i].SellIn < 6)
+                                    {
+                                        if (Items[i].Quality < 50)
+                                        {
+                                            Items[i].Quality = Items[i].Quality + 1;
+                                        }
+                                    }
+                                }
+                            }
                         }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
+
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            Items[i].SellIn = Items[i].SellIn - 1;
                         }
-                    }
+
+                        if (Items[i].SellIn < 0)
+                        {
+                            if (Items[i].Name != "Aged Brie")
+                            {
+                                if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                                {
+                                    if (Items[i].Quality > 0)
+                                    {
+                                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                        {
+                                            Items[i].Quality = Items[i].Quality - 1;
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                                }
+                            }
+                            else
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                        break;
+
                 }
+        }
+
+        private void UpdateBrieItem(Item item) {
+            item.SellIn -= 1;
+            if (item.Quality == 50)
+            {
+                return;
             }
+            item.Quality += 1;
+            return;
         }
     }
 }

dd2ab53e577cf573ba941a52aabf95e70a8281fb
David Halewood <davidnhalewood@hotmail.com>
2018-02-12 13:11:20 +0000

    Refactor AgedBrieTests

--- src/GildedRose.Tests/Resources/AgedBrieTests.cs
+++ src/GildedRose.Tests/Resources/AgedBrieTests.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using GildedRose.Console;
+using NUnit.Framework;
 
 namespace GildedRose.Tests
 {
-    class AgedBrieTests
+    [TestFixture]
+    public class AgedBrieTests
     {
+        public TheGildedRose shop;
+
+        [Test]
+        public void UpdateQuality_ByOne()
+        {
+            var shop = new TheGildedRose(new List<NormalItem>
+                {
+                    new AgedBrie {Name = "Aged Brie", SellIn = 10, Quality = 20},
+                });
+            shop.UpdateQuality();
+            Assert.That(shop.Items[0].Quality, Is.EqualTo(21));
+        }
+
+        [Test]
+        public void UpdateSellIn_ReduceByOne()
+        {
+            var shop = new TheGildedRose(new List<NormalItem>
+                {
+                    new AgedBrie {Name = "Aged Brie", SellIn = 10, Quality = 20},
+                });
+            shop.UpdateQuality();
+            Assert.That(shop.Items[0].SellIn, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void UpdateQuality_Max50()
+        {
+            var shop = new TheGildedRose(new List<NormalItem>
+                {
+                    new AgedBrie {Name = "Aged Brie", SellIn = 10, Quality = 20},
+                });
+
+            for (var i = 0; i < 100; i++)
+            {
+                shop.UpdateQuality();
+            }
+
+            Assert.That(shop.Items[0].Quality, Is.EqualTo(50));
+        }
     }
 }

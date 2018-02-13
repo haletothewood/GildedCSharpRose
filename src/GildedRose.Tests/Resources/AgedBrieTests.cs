a1b940052903ebdaf91f236b0169f2695f390c7a
David Halewood <davidnhalewood@hotmail.com>
2018-02-13 18:18:04 +0000

    Fix System Resource Links

--- src/GildedRose.Tests/Resources/AgedBrieTests.cs
+++ src/GildedRose.Tests/Resources/AgedBrieTests.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using GildedRose.Console;
+using GildedRose.Console.Resources;
 using GildedRose.Console.Resources.Items;
 using NUnit.Framework;
 
@@ -8,42 +8,40 @@ namespace GildedRose.Tests.Resources
     [TestFixture]
     public class AgedBrieTests
     {
+        public TheGildedRose Shop;
+
+        [SetUp]
+        public void Init()
+        {
+            Shop = new TheGildedRose(new List<NormalItem>
+            {
+                new AgedBrie {Name = "Aged Brie", SellIn = 10, Quality = 20},
+            });
+        }
+
         [Test]
         public void UpdateQuality_IncreaseByOne()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-                {
-                    new AgedBrie {Name = "Aged Brie", SellIn = 10, Quality = 20},
-                });
-            shop.Update();
-            Assert.That(shop.Items[0].Quality, Is.EqualTo(21));
+            Shop.Update();
+            Assert.That(Shop.Items[0].Quality, Is.EqualTo(21));
         }
 
         [Test]
         public void UpdateSellIn_ReduceByOne()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-                {
-                    new AgedBrie {Name = "Aged Brie", SellIn = 10, Quality = 20},
-                });
-            shop.Update();
-            Assert.That(shop.Items[0].SellIn, Is.EqualTo(9));
+            Shop.Update();
+            Assert.That(Shop.Items[0].SellIn, Is.EqualTo(9));
         }
 
         [Test]
         public void UpdateQuality_Max50()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-                {
-                    new AgedBrie {Name = "Aged Brie", SellIn = 10, Quality = 20},
-                });
-
             for (var i = 0; i < 100; i++)
             {
-                shop.Update();
+                Shop.Update();
             }
 
-            Assert.That(shop.Items[0].Quality, Is.EqualTo(50));
+            Assert.That(Shop.Items[0].Quality, Is.EqualTo(50));
         }
     }
 }
--- src/GildedRose.Tests/Resources/BackstageTests.cs
+++ src/GildedRose.Tests/Resources/BackstageTests.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using GildedRose.Console;
+using GildedRose.Console.Resources;
 using GildedRose.Console.Resources.Items;
 using NUnit.Framework;
 
@@ -8,76 +8,64 @@ namespace GildedRose.Tests.Resources
     [TestFixture]
     public class BackstageTests
     {
-        [Test]
-        public void UpdateQuality_IncreaseByOne()
+        public TheGildedRose Shop;
+
+        [SetUp]
+        public void Init()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
+            Shop = new TheGildedRose(new List<NormalItem>
             {
                 new Backstage {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 6},
             });
-            shop.Update();
-            Assert.That(shop.Items[0].Quality, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void UpdateQuality_IncreaseByOne()
+        {
+            Shop.Update();
+            Assert.That(Shop.Items[0].Quality, Is.EqualTo(7));
         }
 
         [Test]
         public void UpdateSellIn_ReducebyOne()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-            {
-                new Backstage {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 6},
-            });
-            shop.Update();
-            Assert.That(shop.Items[0].SellIn, Is.EqualTo(14));
+            Shop.Update();
+            Assert.That(Shop.Items[0].SellIn, Is.EqualTo(14));
         }
 
         [Test]
         public void UpdateQuality_IncreaseByTwoIfLessThanTenDays()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-            {
-                new Backstage {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 6},
-            });
-
             for (var i = 0; i < 6; i++)
             {
-                shop.Update();
+                Shop.Update();
             }
 
-            Assert.That(shop.Items[0].Quality, Is.EqualTo(13));
+            Assert.That(Shop.Items[0].Quality, Is.EqualTo(13));
         }
 
         [Test]
         public void UpdateQuality_IncreaseByTwoIfLessThanFiveDays()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-            {
-                new Backstage {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 6},
-            });
-
             for (var i = 0; i < 11; i++)
             {
-                shop.Update();
+                Shop.Update();
             }
 
-            Assert.That(shop.Items[0].SellIn, Is.EqualTo(4));
-            Assert.That(shop.Items[0].Quality, Is.EqualTo(24));
+            Assert.That(Shop.Items[0].SellIn, Is.EqualTo(4));
+            Assert.That(Shop.Items[0].Quality, Is.EqualTo(24));
         }
 
         [Test]
         public void UpdateQuality_DropsToZeroWhenExpired()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-            {
-                new Backstage {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 6},
-            });
-
             for (var i = 0; i < 16; i++)
             {
-                shop.Update();
+                Shop.Update();
             }
 
-            Assert.That(shop.Items[0].Quality, Is.EqualTo(0));
-            Assert.That(shop.Items[0].SellIn, Is.EqualTo(-1));
+            Assert.That(Shop.Items[0].Quality, Is.EqualTo(0));
+            Assert.That(Shop.Items[0].SellIn, Is.EqualTo(-1));
         }
     }
 }

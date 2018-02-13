f8f807ffa84f92c24c74e2e1e22fb5da20b1ee01
David Halewood <davidnhalewood@hotmail.com>
2018-02-13 18:18:43 +0000

    Refactor Conjured Tests

--- src/GildedRose.Tests/Resources/ConjuredTests.cs
+++ src/GildedRose.Tests/Resources/ConjuredTests.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using GildedRose.Console.Resources;
 using GildedRose.Console.Resources.Items;
 using NUnit.Framework;
 
@@ -11,60 +8,52 @@ namespace GildedRose.Tests.Resources
     [TestFixture]
     public class ConjuredItemTests
     {
-        [Test]
-        public void UpdatedQuality_ReduceByOne()
+        public TheGildedRose Shop;
+
+        [SetUp]
+        public void Init()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
+            Shop = new TheGildedRose(new List<NormalItem>
             {
                 new Conjured {Name = "Conjured", SellIn = 10, Quality = 20},
             });
-            shop.Update();
+        }
+        [Test]
+        public void UpdatedQuality_ReduceByOne()
+        {
+            Shop.Update();
 
-            Assert.That(shop.Items[0].Quality, Is.EqualTo(18));
+            Assert.That(Shop.Items[0].Quality, Is.EqualTo(18));
         }
 
         [Test]
         public void UpdatedSellIn_ReduceByTwo()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-            {
-                new Conjured {Name = "Conjured", SellIn = 10, Quality = 20},
-            });
-            shop.Update();
+            Shop.Update();
 
-            Assert.That(shop.Items[0].SellIn, Is.EqualTo(9));
+            Assert.That(Shop.Items[0].SellIn, Is.EqualTo(9));
         }
 
         [Test]
         public void UpdatedQuality_ReducesbyFourIfSellInExpired()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-            {
-                new Conjured {Name = "Conjured", SellIn = 10, Quality = 40},
-            });
-
             for (var i = 0; i < 11; i++)
             {
-                shop.Update();
+                Shop.Update();
             }
 
-            Assert.That(shop.Items[0].Quality, Is.EqualTo(16));
+            Assert.That(Shop.Items[0].Quality, Is.EqualTo(16));
         }
 
         [Test]
         public void UpdatedQuality_NoLowerThanZero()
         {
-            var shop = new TheGildedRose(new List<NormalItem>
-            {
-                new Conjured {Name = "Conjured", SellIn = 10, Quality = 20},
-            });
-
             for (var i = 0; i < 100; i++)
             {
-                shop.Update();
+                Shop.Update();
             }
 
-            Assert.That(shop.Items[0].Quality, Is.EqualTo(0));
+            Assert.That(Shop.Items[0].Quality, Is.EqualTo(0));
         }
     }
 }
